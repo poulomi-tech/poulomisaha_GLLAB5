2de36f1675971564d0a6df7288d24098cc2be246
Poulomi Saha <85102202+poulomi-tech@users.noreply.github.com>
2022-10-29 17:07:19 +0530

    Add files via upload

---
+++ GLLab5/src/main/java/com/gl/service/StudentService.java
@@ -0,0 +1,19 @@
+package com.gl.service;
+
+import java.util.List;
+
+import com.gl.entity.Student;
+
+
+public interface StudentService {
+	public List<Student> findAll();
+
+	public Student findById(int theId);
+
+	public void save(Student thestudent);
+
+	public void deleteById(int theId);
+
+	
+
+}
